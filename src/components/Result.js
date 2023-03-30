0bf25b9dc963a13ce4996102abba0cc35837339d
samrasugu <mokuasamr@gmail.com>
2023-03-30 15:07:44 +0300

    Final Commit

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,46 @@
-const Result = () => {
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const Result = ({ data }) => {
+  const { homeValue, loanAmount, loanTerm, interestRate } = data;
+
+  const totalLoanMonths = loanTerm * 12;
+  const interestPerMonth = interestRate / 100 / 12;
+  const monthlyPayment =
+    (loanAmount *
+      interestPerMonth *
+      (1 + interestPerMonth) ** totalLoanMonths) /
+    ((1 + interestPerMonth) ** totalLoanMonths - 1);
+
+  const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
+
+  const pieChartData = {
+    labels: ['Principle', 'Interest'],
+    datasets: [
+        {
+            label: "Ratio of Principle and Interest",
+            data: [homeValue, totalInterestGenerated],
+            backGroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+            borderColor: ['rgba(255, 99, 132, 1)','rgba(54, 162, 235,1)'],
+            borderWidth: 1,
+        }
+    ]
+  }
   return (
-    <div>
-      <h1>Result</h1>
-    </div>
+    <Stack gap={3}>
+        <Typography textAlign='center' variant="h5">
+            Montly Payment: KES {monthlyPayment.toFixed(2)}
+        </Typography>
+        <Stack direction='row' justifyContent='center'>
+            <div>
+                <Pie data={pieChartData} />
+            </div>
+        </Stack>
+    </Stack>
   );
 };
 
--- src/components/TenureSelect.js
+++ src/components/TenureSelect.js
@@ -1,5 +1,31 @@
-const TenureSelect = () => {
-  return <div>TenureSelect</div>;
+import { InputLabel } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import { FormControl } from "@mui/material";
+import { Select } from "@mui/material";
+
+const TenureSelect = ({ data, setData }) => {
+  const handleChange = (event) => {
+    setData({ ...data, loanTerm: event.target.value });
+  };
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="demo-simple-select-label">Tenure</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={data.loanTerm}
+        label="Tenure"
+        defaultValue={5}
+        onChange={handleChange}
+      >
+        <MenuItem value={5}>5 years</MenuItem>
+        <MenuItem value={10}>10 years</MenuItem>
+        <MenuItem value={15}>15 years</MenuItem>
+        <MenuItem value={20}>20 years</MenuItem>
+        <MenuItem value={25}>25 years</MenuItem>
+      </Select>
+    </FormControl>
+  );
 };
 
 export default TenureSelect;
